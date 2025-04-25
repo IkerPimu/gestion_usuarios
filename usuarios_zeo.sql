b54471678aad4c20ae9e448782362121d567fdfd
IkerPimu <i.pimu@hotmail.com>
2025-04-25 11:38:23 +0200

    Add files via upload

---
+++ usuarios_zeo.sql
@@ -0,0 +1,44 @@
+﻿# SQL-Front 5.1  (Build 4.16)
+
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE */;
+/*!40101 SET SQL_MODE='STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES */;
+/*!40103 SET SQL_NOTES='ON' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
+
+
+# Host: localhost    Database: usuarios_zeo
+# ------------------------------------------------------
+# Server version 5.5.19
+
+CREATE DATABASE `usuarios_zeo` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `usuarios_zeo`;
+
+#
+# Source for table usuarios
+#
+
+CREATE TABLE `usuarios` (
+  `Id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(255) NOT NULL DEFAULT '',
+  `Email` varchar(255) NOT NULL DEFAULT '',
+  `Edad` mediumint(9) NOT NULL DEFAULT '0',
+  `Password` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+#
+# Dumping data for table usuarios
+#
+
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'Iker','i.pimu@hotmail.com',34,'iker34');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
